f83701aff7aa58ad3087c869a234889ce773310e
Juliogarort <julio.garort16@gmail.com>
2025-10-16 17:02:36 +0200

    Primera subida de actividad inicial

---
+++ Programación de Inteligencia Artificial/Iniciando en Python/001- VariablesNumericas-Basicas.py
@@ -0,0 +1,20 @@
+# 1. Declarar variables
+a = 2
+b = 3
+
+# 2. Operaciones
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+exponenciacion = a ** b
+
+# 3. Imprimir resultados
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
+print("Exponenciación (a^b):", exponenciacion)
+
+
+
