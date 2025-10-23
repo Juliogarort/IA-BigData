6bdf6457335d3da536078a51a55efbd9c2753fd4
Juliogarort <julio.garort16@gmail.com>
2025-10-23 20:01:57 +0200

    002

--- PIA-Programación de Inteligencia Artificial/Prácticas Python/3_Trabajando_Bucles/002-factorial.py
+++ PIA-Programación de Inteligencia Artificial/Prácticas Python/3_Trabajando_Bucles/002-factorial.py
@@ -1,6 +1,6 @@
 # Escribe un programa que calcule el factorial de un número dado utilizando un bucle while.
 
-numero = int(input("Introduce un número para calcular su factorial: "))
+numero = int(input("Introduce un numero para calcular su factorial: "))
 factorial = 1
 contador = 1
 while contador <= numero:
