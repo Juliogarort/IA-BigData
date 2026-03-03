b2de252058cd7cb9c0114352199e1e4e8aee9732
Juliogarort <julio.garort16@gmail.com>
2026-03-03 16:11:21 +0100

    archivos

---
+++ MIA-Modelos de Inteligencia Artificial/Robótica/Cinemática_Directa/cd_JulioGarcía.py
@@ -0,0 +1,3 @@
+# Programar en Python la cinemática directa de un brazo robótico de 2 DOF utilizando matrices de transformación homogéneas.
+
+# Genera al menos menos 4 resultados posicionales distintos. 
---
+++ MIA-Modelos de Inteligencia Artificial/Robótica/Cinemática_Inversa/ci_JulioGarcía.py
@@ -0,0 +1,6 @@
+# Implementar un Script en Python que calcule los ángulos de articulación de un 
+# # brazo robótico para alcanzar una posición específica.
+
+
+# Para comprobar su funcionalidad, generar 10 posiciones aleatorias e indicar en formato tabla (matriz) 
+# los ángulos de cada articulación del # brazo robótico.
